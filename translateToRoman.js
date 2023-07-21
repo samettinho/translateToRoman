63721aaf6deee80308ea1574a5f54a094daea7d5
samettinho <smtyilmz@hotmail.com>
2023-07-21 14:35:56 +0300

    first commit

---
+++ translateToRoman.js
@@ -0,0 +1,16 @@
+function translateToRoman(num) {
+const ones=["","I","II","III","IV","V","VI","VII","VIII","IX","X"];
+const tens=["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+const hundreds=["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
+let result="Sayi çevrildi --> ";
+
+const x=Math.floor(num/100);
+const y=Math.floor((num-(x*100))/10);
+const z=num-((x*100)+(y*10));
+
+result+=hundreds[x];
+result+=tens[y];
+result+=ones[z];
+console.log(result);
+}
+translateToRoman(99);
